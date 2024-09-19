42e19539933d0e79092f23e85f12f0451cf60d18
JDeWeese <jdeweese574@icloud.com>
2024-09-18 19:37:24 -0500

    Initial Commit

---
+++ Azimuth9.2/Azimuth9_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Azimuth9_2App.swift
+//  Azimuth9.2
+//
+//  Created by Joseph DeWeese on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Azimuth9_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Azimuth9.2Tests/Azimuth9_2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Azimuth9_2Tests.swift
+//  Azimuth9.2Tests
+//
+//  Created by Joseph DeWeese on 9/18/24.
+//
+
+import Testing
+
+struct Azimuth9_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
